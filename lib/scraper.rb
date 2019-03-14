89b53e70a1a8dbe6ebc70d77a986fb2d82ebf86c
Ron Lynch <lynch.ronald1@gmail.com>
2019-03-14 05:31:40 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require 'pry'
 require_relative './course.rb'
 
 class Scraper
-  
+
     def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -24,16 +24,14 @@ class Scraper
   end
 
   # def get_courses
-  # 
+  #
   # end
-  # 
+  #
   # def make_courses
-  # 
+  #
   # end
-  # 
-  
+  #
+
 end
 
 Scraper.new.get_page
-
-
